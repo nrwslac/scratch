09077b24e0e06335e46187f5af3515d6baf99d63
Nick Waters <nrw@psbuild-rhel7-01.slac.stanford.edu>
2024-02-28 09:24:03 -0800

    ENH: update scratch

--- data-analysis/mono-analysis.py
+++ data-analysis/mono-analysis.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 from scipy import fft
 
 def getdata(filename):
-    with open(filename, newline='') as csvfile:
+    with open(filename, newline='', encoding='utf-8-sig') as csvfile:
         posreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         posdiffsamples = []
         for row in posreader:
             posdiffsamples.append(float(row[0]))
+            #print(row[0])
     print(filename, end=" ")
     return posdiffsamples
 
@@ -34,7 +35,7 @@ def calcFreqPlot(filename, samples, freq):
     xf = fft.fftfreq(N, 1 / freq)[:N//2]
     fig, ax = plt.subplots()
     ax.set_title(filename)
-    ax.plot(xf, 2.0 /N * np.abs(yf[0:N//2]))
+    ax.plot(xf[100:], 2.0/N * np.abs(yf[100:N//2]))
     plt.show()
     return None
 
@@ -48,6 +49,30 @@ def calcFreqPlot(filename, samples, freq):
 #calcRMS(getdata('bothunplugged.csv'), 1000)
 #calcRMS(getdata('mirrorunplugged.csv'), 1000)
 #calcRMS(getdata('gpi-base-encoder-counts.csv'), 1000/150.)
+calcRMS(getdata('mpi-error-enc-19102023.csv'), 1000 * 0.004505)
+calcRMS(getdata('gpi-error-enc-19102023.csv'), 1000 / 150)
+
+calcRMS(getdata('mpi-48unplugged-19102023.csv'), 1000 * 0.004505)
+calcRMS(getdata('gpi-48unplugged-19102023.csv'), 1000 / 150)
+
+calcRMS(getdata('mpi-withgpi-unplugged-19102023.csv'), 1000 * 0.004505)
+calcRMS(getdata('gpi-withgpi-unplugged-19102023.csv'), 1000 / 150)
+
+calcRMS(getdata('mpi-withmpi-unplugged-19102023.csv'), 1000 * 0.004505)
+calcRMS(getdata('gpi-withmpi-unplugged-19102023.csv'), 1000 / 150)
+
 #calcFreqPlot("gpi-base-encoder-diff", getdata('gpi-base-encoder-counts.csv'), 100)
 #calcFreqPlot("mirror-pitch-act-posdiff", getdata('mirror-pitch-act-posdiff.csv'), 500)
-calcFreqPlot("mpi-base-encoder-diff", getdata('mpi-rms-error-base.csv'), 100)
+#calcFreqPlot("mpi-base-encoder-diff", getdata('mpi-rms-error-enc-00.csv'), 100)
+#calcFreqPlot("gpi-base-encoder-diff", getdata('gpi-rms-error-enc-00.csv'), 100)
+calcFreqPlot("mpi-base-encoder-diff", getdata('mpi-error-enc-19102023.csv'), 100)
+calcFreqPlot("gpi-base-encoder-diff", getdata('gpi-error-enc-19102023.csv'), 100)
+
+calcFreqPlot("mpi-48unplugged-encoder-diff", getdata('mpi-48unplugged-19102023.csv'), 100)
+calcFreqPlot("gpi-48unplugged-encoder-diff", getdata('gpi-48unplugged-19102023.csv'), 100)
+
+calcFreqPlot("mpi-withgpi-unplugged", getdata('mpi-withgpi-unplugged-19102023.csv'), 100)
+calcFreqPlot("gpi-withgpi-unplugged", getdata('gpi-withgpi-unplugged-19102023.csv'), 100)
+
+calcFreqPlot("mpi-withmpi-unplugged", getdata('mpi-withmpi-unplugged-19102023.csv'), 100)
+calcFreqPlot("gpi-withmpi-unplugged", getdata('gpi-withmpi-unplugged-19102023.csv'), 100)
--- fft-sample.py
+++ fft-sample.py
@@ -1,41 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import fftpack
+from scipy import fft
 
 def sin(x, amp=1):
     return np.sin(amp * x)
+#number samples
+sample_duration = 30
 
-domain = np.arange(0, 100, 0.25)
+#sample / sec
+sample_freq = 200.0
+N = int(sample_duration * sample_freq)
+x = np.linspace(0.0, sample_duration, int(sample_freq * sample_duration), endpoint=False)
 
-print(domain)
+noise = [np.random.randn(1)[0] for i in range(len(x))]
 
-noise = [np.random.randn(1)[0] for i in range(len(domain))]
-print(noise)
-
-range0 = list(map(sin, domain))
+#pure sin
+pure_sine = np.sin(20 * 2.0 * np.pi * x) + np.sin(60 * 2.0 * np.pi * x) + np.sin(95 * 2.0 * np.pi * x)
 
 fig1, ax1 = plt.subplots()
-
-ax1.plot(domain, range0)
+ax1.plot(x, pure_sine)
 ax1.set_title("pure-sine")
 ax1.set_xlabel("time")
 
-range_sum =[x + y for x, y in zip(range0, noise)]
 
+noisy_sin =[x + y for x, y in zip(pure_sine, noise)]
+
+#sig_fft = fftpack.fft(range_sum, len(range_sum))
 fig2, ax2 = plt.subplots()
-ax2.plot(domain, range_sum)
+ax2.plot(x, noisy_sin)
 
-sig_fft = fftpack.fft(range_sum, len(range_sum))
-amplitude = np.abs(sig_fft)
-power = amplitude**2
-sample_freq = fftpack.fftfreq(len(range_sum), 0.25)
-
-print(len(sample_freq))
-print(len(power))
+yf = fft.fft(noisy_sin)
+xf = fft.fftfreq(int(sample_freq * sample_duration), 1.0 / sample_freq)[:N//2]
 
 fig3, ax3 = plt.subplots()
-ax3.plot(sample_freq, power)
-
+ax3.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
 
 plt.show()
+
+#amplitude = np.abs(sig_fft)
+#power = amplitude**2
+#sample_freq = fftpack.fftfreq(len(range_sum), 0.25)
+
+#print(len(sample_freq))
+#print(len(power))
+
+#fig3, ax3 = plt.subplots()
+#ax3.plot(sample_freq, power)
+
 
